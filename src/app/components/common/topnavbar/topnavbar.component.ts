3dcf5f4cf440df45562d798d69a8d102acbae27a
Mykola Khaurdinov <m.khaurdinov@aspanta.com>
2017-11-27 18:34:53 +0200

    fix helpers

--- src/app/components/common/topnavbar/topnavbar.component.ts
+++ src/app/components/common/topnavbar/topnavbar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { smoothlyMenu } from '../../app.helpers';
+import { smoothlyMenu } from '../../../app.helpers';
 declare var jQuery:any;
 
 @Component({
