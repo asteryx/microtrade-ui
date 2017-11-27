15ffb8b217b652ad6fe378e478d700167403b341
Mykola Khaurdinov <m.khaurdinov@aspanta.com>
2017-11-27 19:00:18 +0200

    fix vuln and index components

---
+++ src/app/components/index.ts
@@ -0,0 +1,10 @@
+export * from './common/layouts/basic.component';
+export * from './common/layouts/blank.component';
+export * from './common/layouts/layouts.module';
+export * from './common/footer/footer.component';
+export * from './common/footer/footer.module';
+export * from './common/navigation/navigation.component';
+export * from './common/navigation/navigation.module';
+export * from './common/topnavbar/topnavbar.component';
+export * from './common/topnavbar/topnavbar.module';
+
